2a26cc9091dd0ecd01f7d3f7e7b05c50f9091e5c
shwetha-2006 <sujatasujata45539@gmail.com>
2026-01-16 09:50:59 +0530

    test_celsius_to_fahrenheit.py

--- test_celsius_to_fahrenheit.py
+++ test_celsius_to_fahrenheit.py
@@ -3,4 +3,5 @@ from celsius_to_fahrenheit import celsius_to_fahrenheit
 def test_celsius():
     assert celsius_to_fahrenheit(10) == 50.0
     assert celsius_to_fahrenheit(5) == 41.0
-    assert celsius_to_fahrenheit(6) == 42.8
+    assert celsius_to_fahrenheit(6) == 42.8  
+
